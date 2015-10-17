d9608f66025d855b224e0e350494f5fc2565c3ca
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2015-10-16 23:54:22 -0600

    October Problem 3
    Prime number problem

--- october_problems.js
+++ october_problems.js
@@ -32,3 +32,7 @@ console.log(total) //Bam!
  after we go through all numbers, begin to loop through array
  add up all the even numbers and put them in total
  return total array
+
+
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
