22fbaf9fa983d628cd784d4a2a579f97f9d2669e
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2015-10-31 17:43:00 -0600

    Last october problem

--- october_problems.js
+++ october_problems.js
@@ -51,3 +51,24 @@ console.log(i);    // now the biggest issue with these kind of problems is size
 }
 var a = 600851475143 ;  //its nice to think about the math, but running it in code can cause problems
 largestPrimeFactor(a)
+
+
+//You've built an in-flight entertainment system with on-demand movie streaming.
+//Users on longer flights like to start a second movie right when their first one ends, but they
+//complain that the plane usually lands before they can see the ending. So you're building a feature for
+//choosing two movies whose total runtimes will equal the exact flight length.
+
+//Write a function that takes an integer flight_length (in minutes) and an array of integers movie_lengths
+//(in minutes) and returns a boolean indicating whether there are two numbers in movie_lengths whose sum equals flight_length.
+
+//When building your function:
+
+//Assume your users will watch exactly two movies
+//Don't make your users watch the same movie twice
+//Optimize for runtime over memory
+movie_lengths = [60,80,90,65,45,75]
+flight_length = 180
+
+function flight(flight_length,movie_lengths){
+  
+}
